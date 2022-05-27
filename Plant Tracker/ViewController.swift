6aea1559aea0c0eb0855144a2e757abd7b654f23
Mark <mark.bin.lu@Gmail.com>
2022-05-27 19:50:52 +1000

    Initial Commit

---
+++ Plant Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Plant Tracker
+//
+//  Created by Mark on 27/5/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
